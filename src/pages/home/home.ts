b3064fcf14f40bb25ebab63ead17350a62c1aa14
Tauqeer1 <tauqeer.shakir@yahoo.com>
2018-02-19 00:47:33 +0500

    testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var google: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef: ElementRef;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    // console.log('this.mapRef', this.mapRef);
+    this.displayMap();
+  }
+
+  displayMap() {
+    const location = new google.maps.LatLng('25.070043', '67.284787');
+
+    const options = {
+      center: location,
+      zoom: 10,
+      streetViewControl: false,
+      mapTypeId: 'roadmap'
+    };
+
+    const map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarker(location, map);
+  }
+
+  addMarker(position, map) {
+    return new google.maps.Marker({
+      position,
+      map
+    })
   }
 
 }
